fc09c0e60c4d14e144308371bfe8d78b2341dc98
serii burduja <seriiburduja@gmail.com>
2025-12-26 18:19:22 +0200

    feat: connected prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    super({ adapter, log: ['info', 'warn', 'error'] });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+      console.log('✅ Prisma connected to MySQL');
+    } catch (error) {
+      console.error('❌ Prisma connection error:', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('🔌 Prisma disconnected from MySQL');
+  }
+}
