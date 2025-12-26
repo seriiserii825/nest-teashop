70eb759aed6daa3f6d215add4e21f15aa265d0e7
serii <seriiburduja@gmail.com>
2025-12-26 20:15:38 +0200

    feat: app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
