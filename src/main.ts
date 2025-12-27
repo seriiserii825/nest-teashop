52ea1eeece8ea728c42cd34a04f6910b87360edb
serii <seriiburduja@gmail.com>
2025-12-27 08:36:43 +0200

    feat: register

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { expand } from 'dotenv-expand';
 import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 expand(config());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // удаляет поля, которых нет в DTO
+      forbidNonWhitelisted: true, // ошибка, если прислали лишнее поле
+      transform: true, // включить class-transformer
+      transformOptions: {
+        enableImplicitConversion: true, // автоматически приводит типы
+      },
+    }),
+  );
   app.enableCors({
     origin: [process.env.CLIENT_URL],
     credentials: true,
