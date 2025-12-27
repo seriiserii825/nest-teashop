f388977ddb558e709fc38be1be7ca02672d124a0
serii <seriiburduja@gmail.com>
2025-12-27 08:08:01 +0200

    feat: auth service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
+  }
+
+  // @HttpCode(200)
+  // @Post('login')
+  // login(@Body() dto: AuthDto) {
+  //   return this.authService.login(dto);
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -10,4 +15,44 @@ export class AuthService {
     private userService: UserService,
     private prisma: PrismaService,
   ) {}
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+    return { user, ...tokens };
+  }
+  async register(dto: AuthDto) {
+    const oldUser = await this.userService.getByEmail(dto.email);
+    if (oldUser) {
+      throw new BadRequestException('User already exists');
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: dto.password,
+      },
+    });
+    const tokens = this.issueTokens(user.id);
+    return { user, ...tokens };
+  }
+
+  issueTokens(userId: string) {
+    const payload = { id: userId };
+    const accessToken = this.jwt.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '15m',
+    });
+    const refreshToken = this.jwt.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '7d',
+    });
+    return { accessToken, refreshToken };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.userService.getByEmail(dto.email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
 }
