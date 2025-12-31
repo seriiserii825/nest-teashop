1c5b6d273343ccfc5b60af1fb31a95b67557e2f3
serii burduja <seriiburduja@gmail.com>
2025-12-31 09:56:23 +0200

    feat: need to continue with product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,9 +1,32 @@
+import { ArrayMinSize, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
-  images: any;
+
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  @IsNotEmpty({ each: true })
+  images: string[];
+
+  @IsString()
+  @IsNotEmpty()
   storeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   categoryId?: string;
+
+  @IsString()
+  @IsNotEmpty()
   colorId?: string;
 }
