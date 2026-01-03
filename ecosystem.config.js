8dff5136321ef96a12a00455caca1c1b83ddb3ad
serii <seriiburduja@gmail.com>
2026-01-03 12:28:30 +0200

    feat: ecosystem to start

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nest-teashop',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
