b51fb33b1ab77b466ea15d420295355a46204fec
serii <seriiburduja@gmail.com>
2026-01-06 11:44:50 +0200

    feat: colors auth

--- src/color/color.controller.ts
+++ src/color/color.controller.ts
@@ -13,11 +13,11 @@ import { UpdateColorDto } from './dto/update-color.dto';
 import { Color } from 'src/entities/color.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@Auth()
 @Controller('colors')
 export class ColorController {
   constructor(private readonly colorService: ColorService) {}
 
+  @Auth()
   @Post(':storeId')
   create(
     @Body() createColorDto: CreateColorDto,
@@ -26,21 +26,25 @@ export class ColorController {
     return this.colorService.create(storeId, createColorDto);
   }
 
+  @Auth()
   @Get()
   findAll(): Promise<Color[]> {
     return this.colorService.findAll();
   }
 
+  @Auth()
   @Get(':colorId')
   findById(@Param('colorId') colorId: string): Promise<Color> {
     return this.colorService.findById(colorId);
   }
 
+  @Auth()
   @Get('store/:storeId')
   findByStoreId(@Param('storeId') storeId: string): Promise<Color[]> {
     return this.colorService.findByStoreId(storeId);
   }
 
+  @Auth()
   @Patch(':colorId')
   update(
     @Param('colorId') colorId: string,
@@ -49,6 +53,7 @@ export class ColorController {
     return this.colorService.update(colorId, updateColorDto);
   }
 
+  @Auth()
   @Delete(':colorId')
   remove(@Param('colorId') colorId: string) {
     return this.colorService.remove(colorId);
